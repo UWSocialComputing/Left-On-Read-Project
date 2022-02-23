54219f348ca26eb38053b0fb55d9fd401e60de2c
Catherine Guevara <cgueva@cs.washington.edu>
2022-02-22 16:30:51 -0800

    Add beginning of app
    Include basic App and overall index and updated a few things in the package.json to get jsx and other things working

---
+++ frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+interface AppState {
+    // TODO
+}
+
+/**
+ * Holds the main extension as a container showing icons of users in the current 'Room'
+ */
+class App extends Component<{}, AppState> { 
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <p id="app-title">Chrome Extension thing</p>
+            </div>
+        );
+    }
+
+}
+
+export default App;
