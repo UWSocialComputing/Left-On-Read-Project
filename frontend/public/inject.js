fca413dcea653fba59fd2d4b8366d604d0e9b8df
Christopher Kang <42285636+christopherkang@users.noreply.github.com>
2022-02-23 17:10:18 -0800

    first edit

---
+++ frontend/public/inject.js
@@ -0,0 +1,25 @@
+// build the banner
+
+
+
+
+// alert('hello world')
+console.log('potato farm')
+
+const newDiv = document.createElement("div");
+newDiv.setAttribute("id","lor-banner");
+
+// set CSS
+
+newDiv.setAttribute("class", ".footer");
+
+// for users in room
+// build faces
+
+document.body.appendChild(newDiv);
+
+// while (being seen)
+// {
+// query for status
+// }
+// 
