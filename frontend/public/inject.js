1acac10b10bf95c5537abbc3b32404395340e575
Christopher Kang <42285636+christopherkang@users.noreply.github.com>
2022-02-26 13:52:09 -0800

    modified / commented roberto's code

--- frontend/public/inject.js
+++ frontend/public/inject.js
@@ -11,16 +11,41 @@ newDiv.setAttribute("id", "lor-banner");
 
 // set CSS
 
-// make the div the first one
 
+// create the debugging div
 newDiv.setAttribute("class", "lor-banner");
-const unDiv = document.createElement("div");
+const unDiv = document.createElement("p");
 unDiv.setAttribute("id", "wpm");
 newDiv.appendChild(unDiv);
+
+STEP = 10; // sigfig; round to 1/STEP
+TIME = 1000; // how frequently to run tracking code, in ms 
+
+let lastTime = Date.now();
+let clicksArray = [];
+
+// upon keystroke
+window.onkeydown = function () {
+    let currTime = Date.now(); // provides date in ms
+    let diffSec = (currTime - lastTime) / 1000;
+    let timeDiff = Math.round(diffSec * STEP) / STEP; // rounds to nearest 1/STEP
+    console.log(timeDiff);
+    clicksArray.push(timeDiff);
+    lastTime = currTime;
+};
+
+// run this at set intervals
+setInterval(function () {
+    document.getElementById("wpm").innerText = clicksArray.length > 0 ? clicksArray.toString() : "sfsfds";
+    clicksArray = [];
+
+    // add put code here
+}, TIME);
 
 // for users in room
 // build faces
 
+// make the div the first one
 document.body.appendChild(newDiv);
 
 // while (being seen)
@@ -29,28 +54,28 @@ document.body.appendChild(newDiv);
 // }
 // 
 
-class ClicksCounter {
-    constructor(time, step) {
-        let lastTime = Date.now();
-        let clicksArray = [];
+// class ClicksCounter {
+//     constructor(time, step) {
+//         let lastTime = Date.now();
+//         let clicksArray = [];
 
-        window.onkeydown = function () {
-            let currTime = Date.now();
-            let timeDiff = Math.round(((currTime - lastTime) / 1000) * step) / step;
-            console.log(timeDiff);
-            clicksArray.push(timeDiff);
-            lastTime = currTime;
-        };
+//         window.onkeydown = function () {
+//             let currTime = Date.now();
+//             let timeDiff = Math.round(((currTime - lastTime) / 1000) * step) / step;
+//             console.log(timeDiff);
+//             clicksArray.push(timeDiff);
+//             lastTime = currTime;
+//         };
 
-        this.resetArray = setInterval(function () {
-            document.getElementById("wpm").innerText = clicksArray;
-            clicksArray = [];
-        }, time);
+//         this.resetArray = setInterval(function () {
+//             document.getElementById("wpm").innerText = clicksArray;
+//             clicksArray = [];
+//         }, time);
 
-        this.getArray = function () {
-            return clicksArray;
-        };
-    }
-}
+//         this.getArray = function () {
+//             return clicksArray;
+//         };
+//     }
+// }
 
-var kCounter = new ClicksCounter(1000, 10);
+// var kCounter = new ClicksCounter(1000, 10);
