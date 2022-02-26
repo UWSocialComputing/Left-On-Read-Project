a673e624b4e90153e33e7a03c518ae524cd538ee
Christopher Kang <42285636+christopherkang@users.noreply.github.com>
2022-02-26 13:25:51 -0800

    (Roberto's code)
    directly copy/pasted roberto's code to move into putting

--- frontend/public/inject.js
+++ frontend/public/inject.js
@@ -14,6 +14,9 @@ newDiv.setAttribute("id", "lor-banner");
 // make the div the first one
 
 newDiv.setAttribute("class", "lor-banner");
+const unDiv = document.createElement("div");
+unDiv.setAttribute("id", "wpm");
+newDiv.appendChild(unDiv);
 
 // for users in room
 // build faces
@@ -25,3 +28,29 @@ document.body.appendChild(newDiv);
 // query for status
 // }
 // 
+
+class ClicksCounter {
+    constructor(time, step) {
+        let lastTime = Date.now();
+        let clicksArray = [];
+
+        window.onkeydown = function () {
+            let currTime = Date.now();
+            let timeDiff = Math.round(((currTime - lastTime) / 1000) * step) / step;
+            console.log(timeDiff);
+            clicksArray.push(timeDiff);
+            lastTime = currTime;
+        };
+
+        this.resetArray = setInterval(function () {
+            document.getElementById("wpm").innerText = clicksArray;
+            clicksArray = [];
+        }, time);
+
+        this.getArray = function () {
+            return clicksArray;
+        };
+    }
+}
+
+var kCounter = new ClicksCounter(1000, 10);
