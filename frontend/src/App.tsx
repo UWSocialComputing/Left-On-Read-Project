b537b6b38d83a6ee1914df57514879b65188777c
Catherine Guevara <cgueva@cs.washington.edu>
2022-03-01 13:45:34 -0800

    Add hot-reload configuration and new styles for Room

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import {Layout} from 'antd';
+import {Space, Card} from 'antd';
 import RoomIcon from './RoomIcon';
 
 interface AppState {
-    // TODO
+    currUsers: Array<RoomIcon>
 }
 
 /**
@@ -14,21 +14,25 @@ class App extends Component<{}, AppState> {
     constructor(props: any) {
         super(props);
         this.state = {
+            currUsers: []
         };
     }
 
     // TODO for later: generate x number of RoomIcon components
     // based on number of people in room atm. hard-coded for now
+    
+    // TODO: GET request to fill currUsers array with users from the current room 
     render() {
         return (
-            <div>
-                <RoomIcon name="John Doe"/>
-                <RoomIcon name="Per Sonne"/>
-                <RoomIcon name="Ash Ketchum"/>
-            </div>
+            <Space direction="vertical">
+                <Card title="Room 1" style={{width: 300}}>
+                    <RoomIcon name="John Doe"/>
+                    <RoomIcon name="Per Sonne"/>
+                    <RoomIcon name="Ash Ketchum"/>
+                </Card>
+            </Space>
         );
     }
-
 }
 
 export default App;
