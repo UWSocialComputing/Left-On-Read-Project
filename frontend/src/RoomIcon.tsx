0ff91a05dcd13158633b5f7bdb466471899d88a1
Catherine Guevara <cgueva@cs.washington.edu>
2022-03-01 16:56:14 -0800

    Start to getting keyboard activity

--- frontend/src/RoomIcon.tsx
+++ frontend/src/RoomIcon.tsx
@@ -8,12 +8,49 @@ interface RoomIconProps {
     avatar: String,     // File name of this person's profile pic
 }
 
+interface RoomIconState {
+    flash: any;
+}
+
 /**
  * Represents an icon of a person in the current 'Room'
  */
-class RoomIcon extends Component<RoomIconProps, {}> {
+class RoomIcon extends Component<RoomIconProps, RoomIconState> {
+    interval: any;
 
-    render() {
+    static BLINK_TIME = 500;
+
+    static flash_off_style = {
+        outline: "none",
+    }
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            flash: RoomIcon.flash_off_style
+        };
+        this.interval = null;
+    }
+
+    componentDidMount() {
+        const roomUrl = "https://twyd.herokuapp.com/room/";
+
+        fetch(roomUrl)
+            .then(response => response.json())
+            .then(data => {
+                var result = data.filter((obj: { name: String; }) => {return obj.name === this.props.name});
+                var keyboard_activity = result['keyboard_activity'];
+                for (var i = 0; i < keyboard_activity.length; i++) {
+                    this.interval = setInterval(() => this.blinkIcon(keyboard_activity[i]), RoomIcon.BLINK_TIME);
+                }
+            });
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.interval);
+    }
+
+    blinkIcon(activity: number) {
         const flash_on_style = {
             outline: "none",
             borderColor: "#3369ff",
@@ -24,12 +61,19 @@ class RoomIcon extends Component<RoomIconProps, {}> {
             outline: "none",
         }
 
-        // conditional check this
+        if (activity === 1) {
+            this.setState({flash: flash_on_style});
+        } else {
+            this.setState({flash: flash_off_style});
+        }
+    }
+
+    render() {
 
         return (
             <div>
                 <Tooltip placement="right" title={<UserInfo name={this.props.name}/>}>
-                    <Avatar size={64} src={this.props.avatar} style={flash_on_style}/>
+                    <Avatar size={64} src={this.props.avatar} style={this.state.flash}/>
                 </Tooltip>
             </div>
         );  
